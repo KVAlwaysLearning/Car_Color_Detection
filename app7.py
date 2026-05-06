ef30f4ee0a87a1adcb1456d77342e406c01aa0e5
KVAlwaysLearning <codingacc979@gmail.com>
2026-05-07 00:26:37 +0530

    Update app7.py

--- app7.py
+++ app7.py
@@ -180,12 +180,12 @@ def process_image(uploaded_file):
                 all_p_boxes.append(box.xyxy[0].cpu().numpy().tolist())
                 all_p_confs.append(float(box.conf[0]))
         
-        p_indices = cv2.dnn.NMSBoxes(all_p_boxes, all_p_confs, 0.30, 0.85)
-        if len(p_indices) > 0:
-            p_count = len(p_indices.flatten())
-            for i in p_indices.flatten():
-                b = all_p_boxes[i]
-                cv2.rectangle(display_img, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (0, 255, 255), 1)
+    p_indices = cv2.dnn.NMSBoxes(all_p_boxes, all_p_confs, 0.30, 0.85)
+    if len(p_indices) > 0:
+        p_count = len(p_indices.flatten())
+        for i in p_indices.flatten():
+            b = all_p_boxes[i]
+            cv2.rectangle(display_img, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (0, 255, 255), 1)
 
     #for b in unique_signals:
     #    cv2.rectangle(display_img, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 0, 255), 1)
